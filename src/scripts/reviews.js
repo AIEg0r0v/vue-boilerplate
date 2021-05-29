4e34c53de92b4207b1a535c3e6cba76946785d47
Alexander Egorov <sandr5705@gmail.com>
2021-05-29 14:29:15 +0200

    Making review slider

---
+++ src/scripts/reviews.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+
+
+
+const works = new Vue({
+    template: "#reviews-list",
+    data() {
+        return {
+            
+        }
+    },
+    computed:{
+
+    },
+    watch:{
+
+    },
+    methods: {
+
+    },
+    created(){
+        
+    }
+});
+
+works.$mount('#reviews-component')

4449d191bd0c348420bf9e267fc32cebb21d9138
Alexander Egorov <sandr5705@gmail.com>
2021-05-29 15:00:50 +0200

    More js, less static layout

--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -1,12 +1,21 @@
 import Vue from "vue";
 
+Vue.component('quote', {
+   template: "#quote",
+   props: ["review"] 
+});
 
+
+Vue.component('reviewer', {
+    template: "#reviewer",
+    props: ["reviewer"] 
+ });
 
 const works = new Vue({
     template: "#reviews-list",
     data() {
         return {
-            
+            reviews: []
         }
     },
     computed:{
@@ -16,10 +25,20 @@ const works = new Vue({
 
     },
     methods: {
-
+        updateImagesPath(reviews){
+            return reviews.map(review => {
+                const imagePath = require(`../images/content/${review.reviewer.avatar}`).default;
+                review.reviewer.avatar = imagePath;
+                return review
+            })
+        }
     },
     created(){
-        
+        const reviews = require("../data/reviews.json");
+        this.reviews = this.updateImagesPath(reviews);
+    },
+    mounted() {
+        console.log(this.reviews);
     }
 });
 

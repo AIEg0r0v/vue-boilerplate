6ac87e63f863b670366f0ff0e43482f32d56486c
Alexander Egorov <sandr5705@gmail.com>
2021-06-12 15:11:08 +0200

    added works section for admin
    but is does not works as it should. Only works once

--- src/admin/pages/works/works.vue
+++ src/admin/pages/works/works.vue
@@ -2,21 +2,50 @@
   .container
     .header
       .title Block "Works"
-   
-      
+    ul.works
+      li().work
+        square-btn( 
+          type="square"
+          title="Отправить" 
+          @click="onWorkCreate" 
+        )
+      li(v-for="work in works").work
+        work(:work="work")      
 
 </template>
 
 <script>
+import work from '../../components/work/work.vue'
+import squareBtn from "../../components/button/types/squareBtn/squareBtn.vue";
+// import squareBtn from "../../components/button/button.vue"; //<- this does not work!!!
 
 export default {
-
+  components: {
+    work, squareBtn
+  },
   data() {
       return {
+        works:[]
       }
   },
-  mounted(){
-
+  methods: {
+    onWorkCreate(){
+      console.log('todo: create work');
+    },
+    updateImagesPath(works){
+      return works.map(work => {
+          var imagePath = require(`../../../images/content/${work.image}`).default;
+          //temp fix
+          imagePath = `http://localhost:8080/${imagePath}`
+          
+          work.image = imagePath;
+          return work
+      })
+    }
+  },
+  created(){
+    const works =  require('../../../data/works.json');
+    this.works = this.updateImagesPath(works);
   }
   
 }
